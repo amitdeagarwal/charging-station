b688b7e4981c9b8d762a0429a4e64aaa95491030
Amit agarwal <amitsgagarwal@gmail.com>
2024-08-09 18:11:03 +0200

    move constant in a seprate file

---
+++ lib/constants.dart
@@ -0,0 +1,12 @@
+// constants.dart
+
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+// Constants for location radius and charging points
+const double radius = 50000; // 50 km in meters
+
+// Locations for charging points
+const LatLng darmstadt = LatLng(48.8728, 8.6512);
+const LatLng mannheim = LatLng(49.4875, 8.4647);
+const LatLng heidelburg = LatLng(49.398348, 8.672433);
+const LatLng muenchen = LatLng(48.1351, 11.5820);
